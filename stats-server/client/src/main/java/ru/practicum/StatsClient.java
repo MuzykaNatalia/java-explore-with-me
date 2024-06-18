48834b17dfe324c9c8761a3ffaee74b359c2f84b
MuzykaNatalia <muzyka-natali@bk.ru>
2024-06-18 23:18:45 +0300

    fix: pom and client

--- stats-server/client/src/main/java/ru/practicum/StatsClient.java
+++ stats-server/client/src/main/java/ru/practicum/StatsClient.java
@@ -1,63 +1,49 @@
 package ru.practicum;
 
-import org.springframework.http.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.lang.Nullable;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
-public class StatsClient {
-    protected final RestTemplate rest;
+@Service
+public class StatsClient extends BaseClient {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final String API_PREFIX_HIT = "/hit";
+    private static final String API_PREFIX_STATS = "/stats";
 
-    public StatsClient(RestTemplate rest) {
-        this.rest = rest;
+    @Autowired
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
     }
 
-    protected <T> ResponseEntity<Object> post(String path, T body) {
-        return makeAndSendRequest(HttpMethod.POST, path, null, body);
+    public ResponseEntity<Object> createHit(EndpointHitDto body) {
+        return post(API_PREFIX_HIT, body);
     }
 
-    protected ResponseEntity<Object> get(String path, @Nullable Map<String, Object> parameters) {
-        return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
-    }
+    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end,
+                                           @Nullable String[] uris, Boolean unique) {
+        String startFormatted = FORMATTER.format(start);
+        String endFormatted = FORMATTER.format(end);
 
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path,
-                                                          @Nullable Map<String, Object> parameters,
-                                                          @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
-        ResponseEntity<Object> serverResponse;
-        try {
-            if (parameters != null) {
-                serverResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
-            } else {
-                serverResponse = rest.exchange(path, method, requestEntity, Object.class);
-            }
-        } catch (HttpStatusCodeException e) {
-            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
-        }
-        return prepareResponse(serverResponse);
-    }
-
-    private HttpHeaders defaultHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        return headers;
-    }
-
-    private static ResponseEntity<Object> prepareResponse(ResponseEntity<Object> response) {
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response;
-        }
-
-        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
-
-        if (response.hasBody()) {
-            return responseBuilder.body(response.getBody());
-        }
-
-        return responseBuilder.build();
+        String path = "?start={start}&end={end}&uris={uris}&unique={unique}";
+        Map<String, Object> parameters = Map.of(
+                "start", startFormatted,
+                "end", endFormatted,
+                "uris", uris != null ? String.join(",", uris) : "",
+                "unique", unique);
+        return get(API_PREFIX_STATS + path, parameters);
     }
 }
